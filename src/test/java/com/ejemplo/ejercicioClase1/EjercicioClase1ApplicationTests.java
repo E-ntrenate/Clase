8fb21a5ca5e1a944d569bf5bdfdc0b668e4e192f
Santiago Rodríguez Vallejo <sarodriguezva@unal.edu.co>
2022-04-14 17:11:44 -0500

    Spring Boot

---
+++ src/test/java/com/ejemplo/ejercicioClase1/EjercicioClase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.ejercicioClase1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioClase1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
